32f1f525d94df07d87b878fa18dcdcb7319fcce3
uuso <47225119+uuso@users.noreply.github.com>
2019-11-09 23:19:52 +0300

    fixed server error

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 import os
 import bottle
 
-bottle.route("/")
+@bottle.route("/")
 def func():
 	return "Ok"
 
 bottle.run(
-	server="gunicorn", host="0.0.0.0",
+	server="gunicorn", 
+	host="0.0.0.0",
 	port = int(os.environ.get("PORT",5000)))
